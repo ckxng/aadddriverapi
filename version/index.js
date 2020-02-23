eacd38af10a57cd087cf86b9b4a66592dacf5f89
Cameron King <cking@ckxng.com>
2020-02-22 19:59:29 -0600

    simplify version response

--- version/index.js
+++ version/index.js
@@ -1,14 +1,12 @@
 'use strict';
 
 /**
- * Returns an object representing the api major version.
+ * Returns the api major version.
  * 
  * /api/version
  */
 module.exports = async function (context, req) {
     return {
-        body: {
-            version: 1
-        }
+        body: "1"
     };
 };
